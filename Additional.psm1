379fe3de5605cccfa7015f29c02d505c073f56c5
reloxx13 <reloxx@interia.pl>
2019-07-22 23:47:23 +0200

    add show/hide build number on desktop

--- Additional.psm1
+++ Additional.psm1
@@ -17,3 +17,18 @@ Function HideSecondsInTaskbar {
 	Write-Output "Hiding seconds in taskbar..."
 	Remove-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "ShowSecondsInSystemClock" -ErrorAction SilentlyContinue
 }
+
+# Show Windows build number and Windows edition (Home/Pro/Enterprise) from bottom right of desktop
+Function ShowBuildNumberOnDesktop {
+	Write-Output "Showing build number on desktop..."
+	If (!(Test-Path "HKCU:\Control Panel\Desktop")) {
+		New-Item -Path "HKCU:\Control Panel\Desktop" | Out-Null
+	}
+	Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name "PaintDesktopVersion" -Type DWord -Value 1
+}
+
+# Hide Windows build number and Windows edition (Home/Pro/Enterprise) from bottom right of desktop
+Function HideBuildNumberOnDesktop {
+	Write-Output "Hiding build number on desktop..."
+	Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name "PaintDesktopVersion" -Type DWord -Value 0
+}
